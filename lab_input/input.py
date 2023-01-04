7ab676863c0f17a26d844d9fb5e5207aa0aad4ec
meovermind <chase.a.brooks540@gmail.com>
2023-01-04 21:21:38 +0000

    join and split

---
+++ lab_input/input.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def main():
+    name = input("Whats your name? ")
+    day = input("What day of the week is it? ")
+
+    print ("Hello," + name + "! Happy "+ day + "!")
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ string_prac.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def main():
+    prac_word = "im cool"
+
+    print(prac_word[::-1])
+
+main()
